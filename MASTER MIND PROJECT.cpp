cb171b644f59734f55f61d02871ab05b9c3d6a98
Abdelfetah Ouwahbi <105524997+AbdelfetahOuwahbi@users.noreply.github.com>
2023-04-24 11:22:46 +0100

    Update MASTER MIND PROJECT.cpp

--- MASTER MIND PROJECT.cpp
+++ MASTER MIND PROJECT.cpp
@@ -1,230 +1,456 @@
+//GAME DEVELOPPED BY ABDELFETAH OUWAHBI - SAADINE ELHIMEUR
+
+//the comments is in french language
+
 #include<stdio.h>
 #include<conio.h>
 #include<stdlib.h>
-#include<unistd.h>
-   int tentatives,T[5],Y[5];
-   int i,j,malPlaces,bienPlaces,optionJocker,blocDeCode,o,a,loading,LIST1,LIST2;
-   char nom[20],nom1[20];
-    
-void Loading(){
-	int count=1;
-	loop:
-		system("cls");
-		printf("LOADING.");
-		for(loading=1; loading<=2; loading++){
-			sleep(1);
-			printf(".");
-		}
-		sleep(1);
-		count++;
-		if(count==3)
-		goto endloop;
-		else
-		goto loop;
-	endloop:
-	printf("\n\n LET'S GET STARTED ..\n\n");
-	sleep(2);
-	system("cls");
-	printf("PRESS ANY KEY");
-	getch();	
-}    
-    
-void jocker(){
-	if(tentatives <= 18 && tentatives > 3 ){
-		printf("\n");
-		printf("TOO MUCH ATTEMPTS \n");
-	    printf("\n");
-			printf("TYPE 1 TO USE THE OPTION JOCKER \n");
-			printf("\n");
-			printf("OR \n");
-			printf("\n");
-			printf("TYPE ANY OTHER NUMBER TO KEEP PLAYING \n");
-		scanf("%d",&optionJocker);
-		if(optionJocker == 1 ){
-		
-			printf("TYPE THE BLOC OF THE NUMBER YOU WANT TO SEE : \n");
-			scanf("%d",&blocDeCode);
-			if( blocDeCode<6 && blocDeCode>2 && blocDeCode<5 ){printf("THE NUMBER PLACED IN THE %drd BLOC IS :%d \n",blocDeCode,T[blocDeCode-1]);}
-				
-			if( blocDeCode<6 && blocDeCode==1 ){printf("THE NUMBER PLACED IN THE %dst BLOC IS :%d \n",blocDeCode,T[blocDeCode-1]);}
-			
-				                                                      //all of those (if) conditions are to put the phrases correctely//
-				                                                      
-			if(blocDeCode<6 && blocDeCode==2){printf("THE NUMBER PLACED IN THE %dnd BLOC IS :%d \n",blocDeCode,T[blocDeCode-1]);}
-			
-			if(blocDeCode<6 && blocDeCode==5){printf("THE NUMBER PLACED IN THE %dth BLOC IS :%d \n",blocDeCode,T[blocDeCode-1]);}
-				
-			if(blocDeCode>=6){printf("YOU HAVE ENTERED A NUMBER HIGHER THAN 5 \n");}
-			tentatives=tentatives-3;	
-		}else{
-			printf("FINISH YOUR GAME,THINK CAREFULY NEXT TIME .. \n");
-			printf("\n\n\n");
-			printf("YOU CAN DO IT .... \n");
-				printf("\n");
-		}
-		
-	}
-}
 
-void Nmbr_destinct_p1(){
-	a=1;
-	while(a!=0){
-	a=0;
-	for(i=0;i<5;i++){
-   	for(o=0;o<5;o++){
-   		if(T[i]==T[o] && o!=i){
-   			a=a+1;
-		   }
-		   }
-	   }
-	   if(a>=1){
-	   	printf("YOU HAVE ENTERED A NUMBER MANY TIMES \n");
-	   	printf("\n");
-	 for(i=0;i<5;i++){
-   	   printf("Le chiffre %d :",i+1);
-   	   scanf("%d",&T[i]);
-   	   }
-}
-}
-}
-void Nmbr_destinct_p2(){
-	a=1;
-	while(a!=0){
-	a=0;
-	for(j=0;j<5;j++){
-   	for(o=0;o<5;o++){
-   		if(Y[j]==Y[o]&&o!=j){
-   			a=a+1;
-		   }
-		   }
-	   }
-	   if(a>=1){
-	   	printf("YOU HAVE ENTERED A NUMBER MANY TIMES \n");
-	   	printf("\n");
-	   	for(j=0;j<5;j++){
-   	   printf("Le chiffre %d :",j+1);
-   	   scanf("%d",&Y[j]);
-   	   }
-	   	}
-	   	}
-}
+int T[5],Y[5],F[5],i,j,o,n,essai,a,e,m=0,f=7,p;
+char nom1[20],nom2[20];
+FILE *fnamescore ;
 
-void Maincode(){
-	//affichage de nom et de code de joueur 1//
-   printf(" VOUS ETES LE JOUEUR 1, DONNER VOTER NOM :");
-   scanf("%s",nom);
+//*********************************
+int Welcome();
+void DEBUT();
+void joker();
+void YouLost();
+void EVERYTHING();
+void ACCEUIL();
+void GRAPHE();
+void BIENPLACEE_MALSPLACEE();
+void LEVEL2();
+void FICHIER();
+//*********************************
+
+//La fonction du niveau 2
+void LEVEL2(){
+   printf("     **YOU'RE NOW AT THE LEVEL TWO **\n");
    printf("\n");
-   printf("%s BIENVENUE AU MASTER MIND, DONNER UN CODE (5 CHIFFRES) \n",nom);
-   printf("\n");
-   
-   for(i=0;i<5;i++){
-   	   printf("Le chiffre %d :",i+1);
-   	   
-   	   scanf("%d",&T[i]); 
-   	} 
-	
-   	   
-   Nmbr_destinct_p1();
-   getch();
-   system("cls");
-    
-   //affichage de nom et de code de joueur 2..//
-   
-   printf(" VOUS ETES LE JOUEUR 2, DONNER VOTER NOM :");
+   printf("     -PLAYER 1 TYPE YOUR NAME : ");
    scanf("%s",nom1);
    printf("\n");
-   printf("%s BIENVENUE AU MASTER MIND, ESSAYER DE TROUVER LE CODE (5 CHIFFRES) \n",nom1);
+   printf("     * %s , ENTER THE SECRET CODE TO START THE GAME .  \n",nom1);
    printf("\n");
-   //..et declaration de nombre de tentative//
-    tentatives=20;
-   while(tentatives>0){
-	tentatives=tentatives-1;
-			for(j=0;j<5;j++){
-   	   printf("Le chiffre %d :",j+1);
-   	   scanf("%d",&Y[j]);
-   	   
-   	  //les nombres bien places// 
-   	} Nmbr_destinct_p2();
-bienPlaces=0;j=0;
-   for(i=0;i<5;i++){
-   	if(T[i]==Y[j]){
-   		bienPlaces=bienPlaces+1;
-   } j=j+1;
-   if(bienPlaces==5){                                                        //ANONCE DE GAGNER !!//
-		  	printf("WELL PLAYED, YOU HAVE FOUND THE RIGHT CODE \n");
-		  	
-		  	goto fin;
-   }
-   }printf("LE NOMBRE DES CHIFFRES BIEN PLACES EST :%d \n",bienPlaces);
-   
-   //les nombres mal places//
-   malPlaces=0;
-   for(i=0;i<5;i++){
-   	for(j=0;j<5;j++){
-   		if(T[i]==Y[j]&&j!=i){
-   			malPlaces=malPlaces+1;
-		   }
-	   }
-   }printf("LE NOMBRE DES CHIFFRES MAL PLACEES EST :%d \n",malPlaces);
-   
-   jocker();  
-   
-   printf("LE NOMBRE DE TENTATIVES RESTANT EST :%d \n",tentatives);
-  
-		  } 
-	if(tentatives==20&&bienPlaces!=5){                                       //ANONCE DU PERT !!//
-		printf("WRONG CODE, INFORTUNATELY YOU LOST !!! \n");
-		printf("YOUR SCORE IS : 0 ");
-	}
-	
-	fin : printf("YOU WIN %s !! \n",nom1);	                          //ANONCE DU GAGNER (2)!!//
-	printf("YOUR SCORE IS :%d \n",tentatives+1);
-  
-  
-   }
 
-void lists(){
-	printf("CLICK 1 TO PLAY \n\n");
-	printf("CLICK 2 TO READ THE GAME'S PRINCIPE \n\n");
-	printf("CLICK 3 TO EXIT \n\n");
-	scanf("%d",&LIST1);
+   for(i=0;i<5;i++){
+   printf("     **NUMBER %d :",i+1);
+   scanf("%d",&T[i]);}
+
+   system("cls");
+   GRAPHE();
+   printf("     **YOU'RE NOW AT THE LEVEL TWO **\n");
+   printf("\n");
+   printf("     -PLAYER 2 TYPE YOUR NAME :");
+   scanf("%s",nom2);
+   printf("\n");
+   printf("     * %s , GAME STARTED .\n",nom2);
+   printf("     -----TRY TO FIND THE CODE");
+   printf("\n");
+    f=9; //Anything but 0 to block le joker
+    essai=20; //initialisation
+	while(essai>0){
+	//sauter une ligne
+   	printf("\n");
+   	//scan des valeurs du 2eme joueurs
+   for(j=0;j<5;j++){
+   	    printf("     **NUMBER %d :",j+1);
+   	    scanf("%d",&Y[j]);
+		   }
+   	BIENPLACEE_MALSPLACEE();
+
+	//sauter une ligne
+	printf("\n");
+
+	//affichege des chiffres bien placees et les mals placees
+    printf("     *WELL PLACED   :%d\n",o);
+    printf("     *BADLY PLACED : %d\n",n);
+
+    //sauter une ligne
+    printf("\n");
+
+    //compteur des essais
+
+    essai=essai-1;
+
+    //la condition pour continuer les tentatives
+    if(o!=5&&essai!=0){
+    printf("     --NUMBER OF TRIALS YOU HAVE LEFT :  %d\n",essai);}
+    //Si le joueur 2 gangne
+    YouLost();
+
+    ///Les instructions si le joueur 2 gagne
+          if(o==5){
+	printf("           YOU WIN !!\n");
+	printf("           ----YOUR SCORE : %d\n",essai);
+	printf("\n");
+	printf("           END OF THE GAME .\n");
+
+	essai=0;
+	printf("\n");
+	//Si le joueur veut rejouer ou quiter apres terminer le jeu
+	printf("     1 : TO EXIT\n");
+	printf("     0 : TO REPLAY\n");
+	printf("\n");
+	printf("     *CHOOSE : ");
+	FICHIER();
+	scanf("%d",&m);
 	system("cls");
-	if(LIST1==1){
-		Loading();
-		Maincode();
-	} 
-	if(LIST1==2){
-	printf("THE GAME'S NAME IS MASTER MIND \n\n");
-	printf("FIRST OF ALL YOU CHOOSE THE LEVEL (1 OR 2) YOU WANT TO PLAY IN \n\n");
-	printf("THE PRINCIPE OF LEVEL 1 IS : \n\n");
-	printf("THE PLAYERS CANNOT ENTER REPEATED NUMBERS \n\n");
-	printf("THE PRINCIPE OF LEVEL 2 IS : \n\n");
-	printf("THE PLAYERS CAN ENTER REPEATED NUMBERS \n\n");
-	printf("THE 1ST PLAYER ENTERS A CODE (5 DIGITS) \n\n");
-	printf("THEN THE 2ND PLAYER TRIES TO GUESS THE CODE (5 DIGITS) \n\n\n");
-	printf("CLICK 1 TO GO BACK \n");
-	printf("CLICK 2 TO EXIT \n\n");
-	scanf("%d",&LIST2);
-	if(LIST2==1){
-		system("cls");
-		lists();
-	}
-	if(LIST2==2){
+	if (m==1){
 		exit(1);
+	}if (m==0){
+	GRAPHE();
+	Welcome();}}
+		joker(); }
+}
+//La fonction qui calcule les biens placees et les mals placees
+void BIENPLACEE_MALSPLACEE(){
+   		   for(i=0;i<5;i++){
+			F[i]=T[i];
+		}
+   	//la boucles des biens placees
+   	  o=0;
+   	  for(i=0;i<5;i++){
+   	  	if(F[i]==Y[i]){o=o+1;
+			 Y[i]=981;
+			 F[i]=971;}
+   	  		}
+	//la boucle des mals placees
+		   n=0;
+		  for(i=0;i<5;i++){
+		  	p=1;
+		  	 for(j=0;j<5;j++){
+		  		if(F[i]==Y[j]&&j!=i&&p!=0){
+		  			Y[j]=981;
+		  			n=n+1;
+					p=0;}
+					  }}
+}
+//la fonction du schema master mind , pour un peu de design
+void GRAPHE(){
+   printf("          %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c\n",30,16,16,16,16,16,16,16,16,16,16,16,16,16,16,16,16,16,16,31);
+   printf("          %c                                     %c   \n",30,31);
+   printf("          %c     M  A  S  T  E  R    M I N D     %c  \n",30,31);
+   printf("          %c     M  A  S  T  E  R    M I N D     %c  \n",30,31);
+   printf("          %c     M  A  S  T  E  R    M I N D     %c   \n",30,31);
+   printf("          %c                                     %c     \n",30,31);
+   printf("          %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c \n",30,17,17,17,17,17,17,17,17,17,17,17,17,17,17,17,17,17,17,31);
+   printf("\n");
+}
+//la fonction du menu
+int Welcome(){
+    printf("     =======THE MAIN MENU=======\n");
+    printf("\n");
+    //une valeur dehors 0 ,1,2 et 3 , pour ne pas fair tromper le scan
+	m=5;
+	p=5;
+    //Main menu
+    printf("      1 : TO PLAY\n");
+    printf("      2 : TO SEE THE MENU\n");
+    printf("      0 : TO CLOSE\n");
+    printf("\n");
+    printf("      *CHOOSE : ");
+    scanf("%d",&m);
+    system("cls");
+    if(m==1){
+    	EVERYTHING();
 	}
-	}
-	if(LIST1==3){
+    if(m==2){
+       //les regles du jeu , une introduction
+		GRAPHE();
+		printf("-MASTERMIND is a code-breaking game for two players. The first one propose \n");
+		printf(" a code of 5 numbers , and the second one should find it , with little  ,  \n");
+		printf(" help which is marking The numbers well placed and the badly placed  , \n");
+		printf(" with 20 chances of playing.\n");
+		printf("\n");
+		printf("There's two levels in this game :\n");
+        printf("\n");
+		printf("---The first , the easy  one based on that the 5 code proposed by  the  first   \n");
+		printf("should be distinct , which is easy for the second player to play.This level\n");
+		printf("is recommended for the kids to have fun playing , not getting mad .   \n");
+        printf("\n");
+		printf("---The second one is the opposite of the last one ,which is allow to the      \n");
+		printf("players to tap a code nondistinct ,nondistinct means the numbers can repeat \n");
+		printf("or not ,it makes the game much harder .\n");
+        printf("\n");
+		printf("-There's is something last to add , if you're having any trouble finding the  \n");
+		printf(" code, the game propose for you an option called 'JOKER' , it's unlocked\n");
+		printf(" after the 10th chances in back you lose 3 chances of playing , it help    \n");
+		printf(" you to see one code of the secret code , but once you use you can't \n");
+		printf(" reuse it , so be carefull using it . \n");
+		printf("\n");
+		printf("   HAVE FUN PALYING .   \n");
+		printf("\n");
+		//Menu pour le retour
+	    printf("    1 : TO GO BACK \n");
+	    printf("    0 : TO CLOSE\n");
+	    printf("\n");
+        printf("    *CHOOSE : ");
+        scanf("%d",&p);
+	    system("cls");
+	       if(p==1){
+	       	GRAPHE();
+	  	    Welcome();
+	       }
+	      if(p==0){
+	  	   exit(1);
+	       }}
+	if(m==0)	{
 		exit(1);
-	}
+	}}
+	//La fonction du execution du jeu
+void EVERYTHING()
+	{
+		GRAPHE();
+           //Menu pour choisir les niveaux
+		    printf("      1  : FOR LEVEL ONE \n");
+		    printf("      2  : FOR LEVEL TWO \n");
+		    printf("      0  : TO GO BACK\n");
+		    printf("\n");
+            printf("      *CHOOSE : ");
+		    scanf("	%d",&m);
+		    system("cls");
+		    GRAPHE();
+		    if(m==0){
+
+		    	Welcome();
+			}
+		    if(m==1){
+    system ("cls");
+
+
+
+	DEBUT();
+       f=9;//pour reinitialiser le blockeur du joker .
+	    essai=20;
+	while(essai>0){
+
+
+	//sauter une ligne
+   	printf("\n");
+
+   	//scan des chiffres du 2eme joueurs
+    for(j=0;j<5;j++){
+   	    printf("     **NUMBER %d :",j+1);
+
+   	    scanf("%d",&Y[j]);
+		   }
+
+     //si l'utilisateur a entrer un chiffre bcp de fois , le programme lui donne pour repeter .
+           a=0;
+		   for(i=0;i<5;i++){
+		  	for (j=0;j<5;j++){
+		  		if(Y[i]==Y[j]&&j!=i){
+		  			a=a+1;
+					  }}}
+		 if ( a>=1){
+        printf("\n");
+		 printf("     !! YOU ENTRED A NUMBER MANY TIMES !! \n");
+		 printf("     --REPLAY.\n");
+		 printf("     --NUMBER OF TRIALS YOU HAVE LEFT : %d\n",essai);
+
+		 i=2;
+		 }
+
+	else{
+
+
+   	BIENPLACEE_MALSPLACEE();
+
+	//sauter une ligne
+	printf("\n");
+
+	//affichege des chiffres bien placees et les mals placees
+    printf("     *WELL PLACED  :%d\n",o);
+    printf("     *BADLY PLACED : %d\n",n);
+
+
+    //sauter une ligne
+    printf("\n");
+
+
+    //compteur des essais
+    essai=essai-1;
+
+    //si tous est rigolo ,et le joueur 2 na ni gagner ni perder
+    if(o!=5&&essai!=0){
+    printf("     --NUMBER OF TRIALS YOU HAVE LEFT :  %d\n",essai);
 }
 
-main(){
-	system("color 5b");
-   lists();
-   Maincode();
-	 
- }
-   
-   
-   
+   //si le joueur 2 perdu
+    YouLost();
 
+    //Les instructions si le joueur 2 gagne
+          if(o==5){
+	printf("           YOU WIN !!\n");
+	printf("           ----YOURE SCORE : %d\n",essai);
+	printf("\n");
+	printf("           END OF THE GAME .\n");
+	essai=0;
+	printf("\n");
+	printf("     1 : TO REPLAY\n");
+	printf("     0 : TO EXIT\n");
+	printf("\n");
+    printf("     *CHOOSE : ");
+    FICHIER();
+	scanf("%d",&m);
+	system("cls");
+	if (m==0){
+		exit(1);
+
+	}if (m==1){
+	GRAPHE();
+	Welcome();
+	}
+	 }
+		}joker(); }}
+		if(m==2){
+			LEVEL2();
+		}
+		}
+
+//La fonction du input pout le niveau 1
+void DEBUT(){
+	GRAPHE();
+	printf("     **YOU'RE NOW AT THE LEVEL 1**\n");
+	printf("\n");
+
+   printf("     -PLAYER 1 TYPE YOUR NAME : ");
+
+   scanf("%s",nom1);
+   printf("\n");
+   printf("     * %s , ENTER THE SECRET CODE TO START THE GAME .  \n",nom1);
+   printf("\n");
+   a=1;
+   while(a!=0){
+   for(i=0;i<5;i++){
+   	   printf("     **NUMBER %d :",i+1);
+   	   scanf("%d",&T[i]);}
+      a=0;
+		   for(i=0;i<5;i++){
+		  	for (j=0;j<5;j++){
+		  		if(T[i]==T[j]&&j!=i){
+		  			a=a+1;}}}
+		 if ( a!=0){
+         printf("\n");
+		 printf("     !! YOU ENTERED A NUMBER MANY TIMES !! \n");
+		 printf("     --REENTER THE CODE .\n");
+		 printf("\n");
+              }
+	     else{
+   system("cls");
+   GRAPHE();
+   printf("     **YOU'RE NOW AT THE LEVEL 1**\n");
+   printf("\n");
+   printf("     -PLAYER 2 TYPE YOUR NAME :");
+
+   scanf("%s",nom2);
+   printf("\n");
+   printf("     * %s , GAME STARTED .\n",nom2);
+
+   printf("     -----TRY TO FIND THE CODE");
+
+   printf("\n");}}
+   }
+
+//La fonction du perdu
+void YouLost(){
+	if(essai==0&&n>=0&&o!=5)
+	{
+	printf("           YOU LOST !!\n");
+	printf("           ----YOURE SCORE : 0\n");
+	printf("\n");
+	printf("           --THE CODE ENTERED BY %s : \n ",nom1);
+
+
+	printf("\n");
+	for(i=0;i<5;i++){
+	printf("                       **NUMBER %d : %d\n",i+1,T[i]);
+}
+     printf("\n");
+	printf("                       --END OF THE GAME .\n");
+
+	printf("\n");
+	printf("     1 : TO EXIT\n");
+	printf("     0 : TO REPLAY\n");
+	printf("\n");
+    printf("          *CHOOSE : ");
+    FICHIER();
+
+	scanf("%d",&m);
+	system("cls");
+	if (m==1){
+		exit(1);
+
+	}if (m==0){
+	GRAPHE();
+	Welcome();
+	}}
+}
+//La fonction du joker
+void joker(){
+
+	if(essai==10&&i!=2){
+    printf("\n");
+	printf("     ------||YOU NOW UNLOCKED THE OPTION JOKER .||------\n ");
+	}
+
+ 	  if(f==0&&essai!=0||essai<=3&&essai!=0){
+ 	  	printf("\n");                                               //le block du joker
+ 	  	printf("     !!YOU CAN'T USE THE OPTION JOKER NOW .\n");
+	   }
+
+ if(essai<=10&&essai>3&&f!=0&&i!=2){
+     printf("\n");
+     printf("     **TYPE '1' IF YOU WANT TO USE THE OPTION JOKER .**\n");
+     printf("\n");
+     printf("     **TYPE ANY OTHER NUMBER IF YOU DONT WANT TO .**\n");
+     printf("\n");
+     printf("       *CHOOSE : ");
+     scanf("%d",&m);
+   	     if(m==1){
+   	     	f=0;
+   	     	if(essai>=4){
+                 printf("\n");
+                 printf("     YOU'RE USING THE OPTION JOKER .\n");
+	             printf("\n");                                             //Si le joueur a decider d'utiliser l'option joker .
+	             printf("     TYPE THE NUMBER YOU WANT TO SEE : \n");
+	             printf("\n");
+                 printf("       *CHOOSE : ");
+                 scanf("%d",&e);
+                 printf("\n");
+	              if(e<6&&e!=0){
+	                  printf("     THE NUMBER YOU CHOOSE : [%d] \n",e);
+                      printf("\n");
+                      printf("     IT VALUE IS : '%d'\n",T[e-1]);
+	                  essai=essai-3;}
+    }else
+	{printf("    YOU JUST ENTERED A NUMBER BIGGER THAN 5 .");}
+	}else {
+    printf("\n");
+	printf("     YOU DIDNT CHOOSE THE OPTION JOKER .");
+	printf("\n");
+	printf("     --FINISH YOUR GAME .");
+	printf("\n");}
+}}
+//le fonction pour stocker les noms et le scores des joueurs
+void FICHIER()
+{
+ //stockage du nom et du score dans un fichier dans votre ordinateur//
+    fnamescore = fopen("history_mastermind.txt","w");
+
+    fprintf(fnamescore,"\n LE JOUEUR %s BIEN JOUER \nVotre score EST : %d\n",nom2,essai);
+
+    fclose(fnamescore);}
+
+ main(){
+    system("color 0d");
+
+	GRAPHE();
+	Welcome();     //Appel des fonction dans la fonction principale main .
+    EVERYTHING();
+
+
+
+    getch();
+}// THANKS FOR READING THE CODE//
